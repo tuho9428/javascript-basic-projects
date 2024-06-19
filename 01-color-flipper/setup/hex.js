6b1800e48eb7d9c4a82fc08b6b415a2fde5db87a
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-06-18 20:03:54 -0700

    01 color

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+    let str = "#";
+    for(let i = 0; i < 6; i++){
+        str += hex[getRandomNumber()];
+    };
+
+    console.log(str);
+    color.textContent = str;
+    document.body.style.backgroundColor = str;
+
+
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+};
